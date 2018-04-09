21dbb043a0c484c8c8351dc4f0e6804749f0f44d
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-09 03:06:09 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ def oxford_comma(array)
   if array.length == 1
   array.join(" , ")
   else
-  array[0..-2].join(", ") + " and " + array.pop
+  array[0..-1].join(", ") + " and " + array.pop
 end
 
 end
