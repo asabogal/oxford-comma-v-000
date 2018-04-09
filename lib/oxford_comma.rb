9e4771038df798a56250d15e1cbbf104b4b1fb44
Alejandro Sabogal <asabogal.git@gmail.com>
2018-04-09 03:30:46 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,8 @@
 def oxford_comma(array)
-  if array.length < 2
-  array.join(" and ")
+  if array.size > 2
+    array[0..-2].join(", ") + ", and " + array.pop
 else
-  array[0..-2].join(", ") + ", and " + array.pop
+  array.join(" and ")
 end
 
 end
